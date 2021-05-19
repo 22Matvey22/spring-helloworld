6c6e523556639ed5122b59c769c8a8164e2eabdf
22Matvey22 <matvey.gaverdov@yandex.ru>
2021-05-19 10:37:27 +0500

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return name;
+    }
+
+    public void setMessage(String name) {
+        this.name = name;
+    }
+}
