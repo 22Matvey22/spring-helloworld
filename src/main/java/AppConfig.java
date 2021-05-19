6863ddfc7113dada4aaf626549c6f2d86d3d8b84
22Matvey22 <matvey.gaverdov@yandex.ru>
2021-05-19 11:01:14 +0500

    Initial commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
 
-    @Bean(name="cat")
+    @Bean(name = "cat")
     @Scope(value = "prototype")
-    public Cat getCat(){
+    public Cat getCat() {
         return new Cat();
     }
 }
